48ac6ea2f10dd237bf40a611a3bc8f9171f3a0b1
Mark Lhester Agellon <kianirel56@gmail.com>
2023-02-16 22:18:29 +0800

    Processing the random question generator

--- flag_questions/application.js
+++ flag_questions/application.js
@@ -1,9 +1,26 @@
+//Select elements
+let countSpan = document.querySelector('.count span');
+let flag_imgDiv = document.querySelector('.flag-image');
+let flag_img = document.querySelector('.flag-image img')
+
+
 function getQuestions(){
     let myRequest = new XMLHttpRequest();
     myRequest.onreadystatechange =function(){
         if(this.readyState === 4 && this.status === 200){
             let questions = JSON.parse(this.responseText)
+            console.log(questions)
+            // Number of items per region
+            let qCount = 1;
+            questionNum(qCount)
         }
     }
-    
+    myRequest.open("GET", "questions.json", true);
+    myRequest.send();
+
+}
+getQuestions();
+
+function questionNum(num){
+    countSpan.innerHTML = num
 }
