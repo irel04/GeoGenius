42710e5f289e2a6c3bcf654e069415e8a7f65ef3
Mark Lhester Agellon <kianirel56@gmail.com>
2023-02-17 16:32:32 +0800

    Implemented js to html

--- flag_questions/application.js
+++ flag_questions/application.js
@@ -1,7 +1,10 @@
 //Select elements
 let countSpan = document.querySelector('.count span');
 let flag_imgDiv = document.querySelector('.flag-image');
-let flag_img = document.querySelector('.flag-image img')
+let flag_img = document.querySelector('.flag-image img');
+let flagOptions = document.querySelectorAll('.flag-options ul');
+let flagLis = document.querySelectorAll('.flag-options ul li');
+let currentIndex=0;
 
 
 function getQuestions(){
@@ -13,6 +16,8 @@ function getQuestions(){
             // Number of items per region
             let qCount = 1;
             questionNum(qCount)
+            // call the function for generate questions
+            generateQuestion(questions[currentIndex], qCount)
         }
     }
     myRequest.open("GET", "questions.json", true);
@@ -23,4 +28,19 @@ getQuestions();
 
 function questionNum(num){
     countSpan.innerHTML = num
+}
+
+function generateQuestion(obj, count){
+    if(currentIndex < count){
+        flag_img.src= `Africa/Flags/${obj.img}`;
+        // for generating options
+        flagLis.forEach((li, i) => {
+            // dynamic id of every list
+            li.id = `answer_${i+1}`
+            // dynamic attribute of list
+            li.dataset.answer=obj[`options`][i]
+            // insert the option in the li
+            li.innerHTML = obj[`options`][i];
+        })
+    }
 }
