df5d3e53b67b6c3bbc7e47a8f4dfebcb59ce65c5
Mark Lhester Agellon <kianirel56@gmail.com>
2023-02-20 09:47:44 +0800

    Implementing map category for questions

--- flag_questions/application.js
+++ flag_questions/application.js
@@ -8,166 +8,176 @@ let score = document.querySelector('h3 span');
 let scoreDiv = document.querySelector('.score');
 let correctAns = document.querySelector('.score .right span');
 let chosen_region = "Africa"
+let choosenCategory = "Flags"
 
-function flag_category(){ 
-     let currentIndex=0;
-     let rightAnswer = 0;
-     let qCount = 1;
- 
-     //Africa array for the question
-     let Africa = [];
- 
-     // Asia array for the question
-     let Asia =[];
- 
-     // Europe array for the question
-     let Europe =[];
- 
-     // North_South_America array for the question
-     let North_South_America =[];
- 
-     // Ocenia array for the question
-     let Oceania =[];
- 
-     function getQuestions(){
-         let myRequest = new XMLHttpRequest();
-         myRequest.onreadystatechange =function(){
-             if(this.readyState === 4 && this.status === 200){
-                 let questions = JSON.parse(this.responseText)
-                 //Shuffling the questions
-                 
-                 if(chosen_region == "Africa"){
-                     region_selector(questions)
-                     questions = Africa.sort(() => Math.random() - Math.random()).slice(0, 10)
-                 }
-                 else if (chosen_region == "Asia"){
-                     region_selector(questions)
-                     questions = Asia.sort(() => Math.random() - Math.random()).slice(0, 10)
-                 }
-                 else if (chosen_region == "Europe"){
-                     region_selector(questions)
-                     questions = Europe.sort(() => Math.random() - Math.random()).slice(0, 10)
-                 }
-                 else if (chosen_region == "North and South America"){
-                     region_selector(questions)
-                     questions = North_South_America.sort(() => Math.random() - Math.random()).slice(0, 10)
-                 }
-                 else if (chosen_region == "Oceania"){
-                     region_selector(questions)
-                     questions = Oceania.sort(() => Math.random() - Math.random()).slice(0, 10)
-                 }
-                 
-                 // call the function for generate questions
-                 generateQuestion(questions[currentIndex], qCount, chosen_region);
-                 questionNum(qCount);
- 
-                 QLis.forEach(li => {
-                     li.addEventListener('click', () => {
-                         let rightAnswer = questions[currentIndex].right_answer;
-                         li.classList.add('active');
-                         
-                         // Number of items per region
-                         
-                         // increment index and the number of the item (qcount)
-                         currentIndex++;
-                         if (qCount < 10) qCount++;
-                         
-                         // Check answer after 500ms
-                         setTimeout(()=>{
-                             check_answer(rightAnswer, qCount);
-                             questionNum(qCount)
-                         }, 500);
- 
-                         setTimeout(()=>{
-                             if (qCount < 10){
-                                 //Remove previous image 
-                                 question_image.src='';
-                                 // Remove All Classes 
-                                 li.classList.remove('active');
-                                 li.classList.remove('success');
-                                 li.classList.remove('wrong');
-                                 // Add questions again
-                                 generateQuestion(questions[currentIndex], qCount, chosen_region);
-                             }
-                             else (()=>{
- 
-                             })
-                             
-                         }, 1000)
-                     })                
-                 })
-             }
-         }
-         myRequest.open("GET", "questions.json", true);
-         myRequest.send();
- 
-     }
-     getQuestions();
- 
-     function questionNum(num){
-         countSpan.innerHTML = num
-     };
- 
-     // This is for iterating the needed questions base on the user input
-     function region_selector(questions){
-         if (chosen_region == "Africa"){
-             for (let i = 0; i < 10; i++){
-                 Africa.push(questions[i])
-             }
-         }
-         else if (chosen_region == "Asia"){
-             for (let i = 10; i < 20; i++){
-                 Asia.push(questions[i])
-             }
-         }
-         else if (chosen_region == "Europe"){
-             for (let i = 20; i < 30; i++){
-                 Europe.push(questions[i])
-             }
-         }
-         else if (chosen_region == "North and South America"){
-             for (let i = 30; i < 40; i++){
-                 North_South_America.push(questions[i])
-             }
-         }
-         else if (chosen_region == "Oceania"){
-             for (let i = 40; i < 50; i++){
-                 Oceania.push(questions[i])
-             }
-         }
-         }
-         
- 
-     function generateQuestion(obj, count, region){
-         if(currentIndex < count){
-             question_image.src= `${region}/Flags/${obj.img}`;
-             // for generating options
-             QLis.forEach((li, i) => {
-                 // dynamic id of every list
-                 li.id = `answer_${i+1}`
-                 // dynamic attribute of list
-                 li.dataset.answer=obj[`options`][i]
-                 // insert the option in the li
-                 li.innerHTML = obj[`options`][i];
-             })
-         }
-     }
- 
-     function check_answer(rAnswer, count){
-         let choosenAnswer;
-         for (let i=0; i<QLis.length; i++){
-             if (QLis[i].classList.contains('active')){
-                 choosenAnswer = QLis[i].dataset.answer;
-                 if (rAnswer == choosenAnswer){;
-                     QLis[i].classList.add('success');
-                     rightAnswer++;
-                     score.innerHTML = rightAnswer;
-                 }else {
-                     QLis[i].classList.add('wrong');
-                 }
-             }
-         }  
-     }
+let currentIndex=0;
+let rightAnswer = 0;
+let qCount = 1;
+
+//Africa array for the question
+let Africa = [];
+
+// Asia array for the question
+let Asia =[];
+
+// Europe array for the question
+let Europe =[];
+
+// North_South_America array for the question
+let North_South_America =[];
+
+// Ocenia array for the question
+let Oceania =[];
+
+function getQuestions(){
+    let myRequest = new XMLHttpRequest();
+    myRequest.onreadystatechange =function(){
+        if(this.readyState === 4 && this.status === 200){
+            let questions = JSON.parse(this.responseText)
+            //Shuffling the questions
+            
+            if(chosen_region == "Africa"){
+                region_selector(questions, choosenCategory)
+                questions = Africa.sort(() => Math.random() - Math.random()).slice(0, 10)
+            }
+            else if (chosen_region == "Asia"){
+                region_selector(questions, choosenCategory)
+                questions = Asia.sort(() => Math.random() - Math.random()).slice(0, 10)
+            }
+            else if (chosen_region == "Europe"){
+                region_selector(questions, choosenCategory)
+                questions = Europe.sort(() => Math.random() - Math.random()).slice(0, 10)
+            }
+            else if (chosen_region == "North and South America"){
+                region_selector(questions, choosenCategory)
+                questions = North_South_America.sort(() => Math.random() - Math.random()).slice(0, 10)
+            }
+            else if (chosen_region == "Oceania"){
+                region_selector(questions, choosenCategory)
+                questions = Oceania.sort(() => Math.random() - Math.random()).slice(0, 10)
+            }
+            
+            // call the function for generate questions
+            generateQuestion(questions[currentIndex], qCount, chosen_region, choosenCategory);
+            questionNum(qCount);
+
+            QLis.forEach(li => {
+                li.addEventListener('click', () => {
+                    let rightAnswer = questions[currentIndex].right_answer;
+                    li.classList.add('active');
+                    
+                    // Number of items per region
+                    
+                    // increment index and the number of the item (qcount)
+                    currentIndex++;
+                    if (qCount < 10) qCount++;
+                    
+                    // Check answer after 500ms
+                    setTimeout(()=>{
+                        check_answer(rightAnswer, qCount);
+                        questionNum(qCount)
+                    }, 500);
+
+                    setTimeout(()=>{
+                        if (qCount < 10){
+                            //Remove previous image 
+                            question_image.src='';
+                            // Remove All Classes 
+                            li.classList.remove('active');
+                            li.classList.remove('success');
+                            li.classList.remove('wrong');
+                            // Add questions again
+                            generateQuestion(questions[currentIndex], qCount, chosen_region, choosenCategory);
+                        }
+                        else (()=>{
+
+                        })
+                        
+                    }, 1000)
+                })                
+            })
+        }
+    }
+    myRequest.open("GET", "questions.json", true);
+    myRequest.send();
+
+}
+getQuestions();
+
+function questionNum(num){
+    countSpan.innerHTML = num
+};
+
+// This is for iterating the needed questions base on the user input
+function region_selector(questions, category){
+    // THis will determine what intervals should be selected on the qjson
+    let q_range = 0
+    let starting_range = 0
+    if(category == "Flags"){
+        q_range = 10;
+    }
+    else if(category == "Maps"){
+        q_range = 60;
+        starting_range = 50
+    }
+    
+    // This will separate questions based on the set interval of the choosen category
+    if (chosen_region == "Africa"){
+        for (starting_range; starting_range < q_range; starting_range++){
+            Africa.push(questions[starting_range])
+        }
+    }
+    else if (chosen_region == "Asia"){
+        for (starting_range + 10; starting_range < q_range + 10; starting_range++){
+            Asia.push(questions[starting_range])
+        }
+    }
+    else if (chosen_region == "Europe"){
+        for (starting_range + 20; starting_range < q_range + 20; starting_range++){
+            Europe.push(questions[starting_range])
+        }
+    }
+    else if (chosen_region == "North and South America"){
+        for (starting_range + 30; starting_range < q_range + 30; starting_range++){
+            North_South_America.push(questions[starting_range])
+        }
+    }
+    else if (chosen_region == "Oceania"){
+        for (starting_range + 40; starting_range < q_range + 40; starting_range++){
+            Oceania.push(questions[starting_range])
+        }
+    }
+    }
+    
+
+function generateQuestion(obj, count, region, category){
+    if(currentIndex < count){
+        question_image.src= `${region}/${category}/${obj.img}`;
+        // for generating options
+        QLis.forEach((li, i) => {
+            // dynamic id of every list
+            li.id = `answer_${i+1}`
+            // dynamic attribute of list
+            li.dataset.answer=obj[`options`][i]
+            // insert the option in the li
+            li.innerHTML = obj[`options`][i];
+        })
+    }
 }
 
-flag_category();
+function check_answer(rAnswer, count){
+    let choosenAnswer;
+    for (let i=0; i<QLis.length; i++){
+        if (QLis[i].classList.contains('active')){
+            choosenAnswer = QLis[i].dataset.answer;
+            if (rAnswer == choosenAnswer){;
+                QLis[i].classList.add('success');
+                rightAnswer++;
+                score.innerHTML = rightAnswer;
+            }else {
+                QLis[i].classList.add('wrong');
+            }
+        }
+    }  
+};
+
